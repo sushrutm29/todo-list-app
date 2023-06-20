6bdbc7073bccaca53c4b8d7a5b982d2089e99ffb
sushrutm29 <smadhavi@stevens.edu>
2023-06-20 00:22:26 -0400

    added firebase hosting files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { Redirect, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Error404 from './components/Error404'
+import Login from './components/auth/Login';
+import Error404 from './components/Error404';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => ( <Redirect to="/home"/>)}/>
           <Route exact path="/home" component={Home} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="*" component={Error404} />
         </Switch>
       </div>
---
+++ src/components/auth/Login.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { db } from "../../firebase/firebase_config";
+
+function Login(props) {
+    return (
+        <div>Login</div>
+    )
+}
+
+export default Login;
